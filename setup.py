ec7202d28dfef4396d573e7e23b865b68a645967
Ivan Galin <19488320+i8enn@users.noreply.github.com>
2023-11-12 13:28:54 +0300

    hotfix: bump mypy version for support PEP 695

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ from setuptools import setup
 setup(
     name='pre_commit_placeholder_package',
     version='0.0.0',
-    install_requires=['mypy==1.7.0'],
+    install_requires=[
+        'mypy @ git+https://github.com/python/mypy.git'
+    ],
 )
